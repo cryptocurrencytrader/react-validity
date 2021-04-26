216cf8a0cb72b6e69dffafa99511ccaec16cc1aa
Dennis Alves <dennis.alves@live.com>
2021-04-26 17:54:30 -0300

    Fix scripts

--- src/EventEmitter.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var wolfy87_eventemitter_1 = require("wolfy87-eventemitter");
-exports["default"] = wolfy87_eventemitter_1["default"];
--- src/context.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var react_1 = require("react");
-var EventEmitter_1 = require("./EventEmitter");
-var Context = react_1["default"].createContext({
-    eventEmitter: new EventEmitter_1["default"]()
-});
-Context.displayName = "ValidityContext";
-exports["default"] = Context;
